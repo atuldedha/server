cfb7da86954352ab21d38564152955149b6a3d5c
Atul Choudhary <t-atul1.choudhary@paytmmoney.com>
2023-06-03 13:33:34 +0530

    allowed origins updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "https://server-gilt-mu.vercel.app/",
+  "https://vaisage.com/",
+  "https://sai-project.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
